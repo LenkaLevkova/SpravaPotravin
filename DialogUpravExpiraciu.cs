6dcb63dcc0e74d0a97c9a64097cb9e32c385d47f
Lenka Levková <lenkalevkova@gmail.com>
2021-05-14 10:00:23 +0200

    Zmazanie zbytocnych instanci v Dialogu UpravExpiraciu

--- DialogUpravExpiraciu.cs
+++ DialogUpravExpiraciu.cs
@@ -5,13 +5,9 @@ namespace SpravaPotravin
 {
     public partial class DialogUpravExpiraciu : Form
     {
-        DialogPridaniePotravin dialogPridat;
-        Jedlo jedlo;
         public DialogUpravExpiraciu()
         {
             InitializeComponent();
-            dialogPridat = new DialogPridaniePotravin();
-            jedlo = dialogPridat.VratJedlo();
         }
         private void DialogUpravExpiraciu_Load(object sender, EventArgs e)
         {
